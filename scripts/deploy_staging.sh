06003684bf66fda062e709177a267b2a1824dc82
Kevin Fu <kevin.fu808@gmail.com>
2016-06-20 16:39:18 -0500

    trying auto deploy for other branches

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -2,8 +2,8 @@
 PATH=$PATH:/home/ubuntu/.nvm/versions/node/v4.4.5/bin
 
 cd /srv/Entree.Server
-git checkout staging
+git checkout $1
 git pull
 npm install
 grunt compile
-pm2 restart index.compiled.js --name "staging"
+pm2 restart index.compiled.js --name "$1"
