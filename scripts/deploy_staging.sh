9947beb81cd35d7379ad4fa976ebfa5bfb3e5c23
Jesse Mao <lt.jmao@gmail.com>
2016-06-20 13:50:10 -0500

    Updated it to remote command

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -2,7 +2,7 @@
 
 cd /srv/Entree.Server
 git pull git@github.com:siddharthgutta/Entree.Server.git
-git checkout master
+git checkout staging
 npm install
 grunt compile
 pm2 restart index.compiled.js --name "staging"
