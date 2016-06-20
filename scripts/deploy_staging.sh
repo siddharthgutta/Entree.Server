6782bc18761977cca409624de5f9fac6810ae6e7
Jesse Mao <lt.jmao@gmail.com>
2016-06-20 11:29:47 -0500

    Added deploy scripts

---
+++ scripts/deploy_staging.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+cd /srv/Entree.Server
+git pull git@github.com:siddharthgutta/Entree.Server.git
+git checkout master
+npm install
+grunt compile
+pm2 restart index.compiled.js --name "staging"
