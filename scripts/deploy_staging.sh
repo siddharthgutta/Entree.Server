9d250c823d9af682bd615e53fad9101ac84c9094
Kevin Fu <kevin.fu808@gmail.com>
2016-06-21 17:05:53 -0500

    testing node_modules in the parent directory

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -4,15 +4,13 @@ PATH=$PATH:/home/ubuntu/.nvm/versions/node/v4.4.5/bin
 cd /home/ubuntu/.pm2/repos
 
 # Check if the folder/branch folder already exists
-if cd $1; then
+if cd $1"Entree.Server"; then
     # Pull if folder exists
-    cd Entree.Server
     git pull
 else
     # If folder doesn't exist, create a new folder and clone into it
-    mkdir $1 && cd $1
-    git clone git@github.com:siddharthgutta/Entree.Server.git
-    cd Entree.Server
+    git clone git@github.com:siddharthgutta/Entree.Server.git $1"Entree.Server"
+    cd $1"Entree.Server"
 fi
 
 # Function to get a free port
